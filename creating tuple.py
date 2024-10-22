6afa11bf9b402d95b9de9e3257af3447125cdc93
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-10-23 05:25:34 +0530

    Add files via upload

---
+++ creating tuple.py
@@ -0,0 +1,14 @@
+a=(0,1,2,3,2,3,2,3,2,3,6,"hinderance")
+b=(0,1,2,3,2,3,2,3,2,1,2,6,"hinderance")
+d="harry","india",25
+name,country,age=d          #we can store and print tuple elements in different variables
+print(type(a))              #to find type of the variable (a)
+print(a[0:])
+print(a[2:5])               #slicing
+print(a.count(2))           #function to count the occurence of a specific element in tuple 
+print(a.index(2))           #to find the first index of any specific element  
+ab=a+b                      #adding 2 tuples in ab
+print(ab)
+c=a*3                       #it repeats (a) 3 times
+print(c)
+print(name)
